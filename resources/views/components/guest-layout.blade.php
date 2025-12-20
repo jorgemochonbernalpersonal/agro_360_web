f35e90b81aea8f4543599f7f235a7b13d0d58c35
jorgemochonbernalpersonal <jorgemochonbernalgitpersonal@gmail.com>
2025-12-20 02:36:28 +0100

    p

---
+++ resources/views/components/guest-layout.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Agro365</title>
+    
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" href="{{ asset('images/logo.png') }}">
+    <link rel="shortcut icon" type="image/png" href="{{ asset('images/logo.png') }}">
+    
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600,700" rel="stylesheet" />
+    
+    <!-- Styles / Scripts -->
+    @livewireStyles
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="bg-gradient-to-br from-[var(--color-agro-green-bg)] via-white to-[var(--color-agro-green-bright)]/30 min-h-screen">
+    <!-- Main Content -->
+    <main class="min-h-screen transition-all duration-300" id="main-content">
+        <div class="p-0">
+            {{ $slot }}
+        </div>
+    </main>
+    
+    @livewireScripts
+</body>
+</html>
+
