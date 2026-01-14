e098c7fd9b78293ff7334f7e9b4595bb2d359f93
jorgemochonbernalpersonal <jorgemochonbernalgitpersonal@gmail.com>
2026-01-14 21:11:47 +0100

    p

--- resources/views/livewire/viticulturist/containers/create.blade.php
+++ resources/views/livewire/viticulturist/containers/create.blade.php
@@ -2,6 +2,7 @@
     $icon = '<svg class="w-10 h-10 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4"/></svg>';
 @endphp
 
+<div>
 <x-form-card
     title="Nuevo Contenedor"
     description="Registra un nuevo contenedor para tu bodega"
@@ -298,3 +299,4 @@
     });
 </script>
 @endscript
+</div>
