d75c38406ab90c874c1473e039e8852c9aaad37c
jorgemochonbernalpersonal <jorgemochonbernalgitpersonal@gmail.com>
2026-03-15 01:52:51 +0100

    temp: remove opcache clear helper

--- public/opcache_clear.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (opcache_reset()) {
-    echo "OPcache cleared OK";
-} else {
-    echo "opcache_reset failed or disabled";
-}
-// Self-destruct attempt (may not work if no write permission)
-@unlink(__FILE__);
